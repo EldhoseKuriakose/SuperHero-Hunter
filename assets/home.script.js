335df4bb5bd67a3b1b10d16c34b7315a99e5167f
Eldhose Kuriakose <eldhosekuriakose96@gmail.com>
2020-09-21 09:21:20 +0530

    done with home and favourites

--- assets/home.script.js
+++ assets/home.script.js
@@ -1,9 +1,15 @@
 var homeButton = document.getElementById("home-button");
 var favouriteButton = document.getElementById("favourite-button");
 var tabno = 1;
-
 var homeContainer = document.getElementById("home-container");
 var favouritesContainer = document.getElementById("favourites-container");
+var characterList = document.getElementById("character-list");
+var favouritesList = [];
+var favouritesArray = JSON.parse(localStorage.getItem("favouritesList"));
+
+if(favouritesArray != null) {
+    favouritesList = favouritesArray;
+}
 
 var searchBox = document.getElementById("search-box");
 let ans = {};
@@ -11,7 +17,7 @@ let results = [];
 
 var searchList = document.getElementById("search-list");
 
-
+//Switching tab to home
 function changeTabHome() {
     if(tabno != 1) {
         homeContainer.style.display = "block";
@@ -20,14 +26,18 @@ function changeTabHome() {
     }
 }
 
+//Switching tab to favourites
 function changeTabFavourite() {
     if(tabno != 2) {
         homeContainer.style.display = "none";
         favouritesContainer.style.display = "block";
         tabno = 2;
+        characterList.innerHTML = "";
+        loadCharacters();
     }
 }
 
+//Searching a hero
 const searchHero = async() => {
     let search = searchBox.value;
     try {
@@ -40,6 +50,13 @@ const searchHero = async() => {
     }
 }
 
+//Adding hero to favourites
+function addToFavorites(character) {
+    favouritesList.push(character);
+    localStorage.setItem("favouritesList", JSON.stringify(favouritesList));
+}
+
+//Displaying heroes when searched
 const displayCharacters = (characters) => {
     const htmlString = characters.map((character) => {
             return `
@@ -59,4 +76,46 @@ const displayCharacters = (characters) => {
     searchList.innerHTML = htmlString;
 };
 
+//Loading characters in favourites
+const loadCharacters = async () => {
+    favouritesList.map(async (id) => {
+        try {
+            const res = await fetch(`https://www.superheroapi.com/api.php/1747613848727847/${id}`);
+            character = await res.json();
+            const htmlString = document.createElement('div');
+            htmlString.id = id;
+            htmlString.innerHTML = (
+                `
+                    <li class="character">
+                        <a href="./profile.html?id=${character.id}">
+                            <h2 id="character-name">${character.name}</h2>
+                        </a>
+                        <p id="character-id">id: ${character.id}</p>
+                        <img class="character-image" src="${character.image.url}"></img>
+                        <p class="fullname">${character.appearance.gender}</p>
+                
+                        <p class="publisher">${character.biography.publisher}</p>
+                        <button class="remove-from-favourites" data-id=${character.id} onClick="removeCharacter(${character.id})">
+                            Remove
+                        </button>
+                                
+                    </li>
+                
+                `
+            );
+            characterList.appendChild(htmlString)
+        } catch (err) {
+            console.error(err);
+        }
+    });
+};
+ 
+//Removing a character from favourites
+const removeCharacter = (id) => {
+    favouritesList.pop(id);
+    document.getElementById(id).remove();
+    localStorage.setItem("favouritesList", JSON.stringify(favouritesList));
+}
+
+//Searchbox listener
 searchBox.addEventListener("keyup", searchHero);
